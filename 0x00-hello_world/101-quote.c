80523c1593220f70fbd2f535a23e75ac0d116013
kkrasta <basitmohammed362@gmail.com>
2023-07-13 12:56:16 -0700

    hi

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "and that piece of art is useful"
+ *              - Dora Korpar, 2015-10-19 followed by a new line to
+ *              the standard error stream using the write function.
+ * Return: 1 (Error)
+ */
+int main(void)
+{
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, strlen(message));
+    return (1);
+}
+
