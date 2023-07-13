20ebc1a6e6f60619f412e9a25fa2e98cb1f9fb4c
kkrasta <basitmohammed362@gmail.com>
2023-07-13 13:57:54 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,9 +11,7 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" -"
-	"Dora Korpar, 2016-10-19\n";
-write(2, message, strlen(message));
+write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
 return (1);
 }
 
