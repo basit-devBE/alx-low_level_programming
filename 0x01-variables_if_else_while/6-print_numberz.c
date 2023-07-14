0c65d3afa290f4556ca1e970c00ce5b3b3bedfde
kkrasta <basitmohammed362@gmail.com>
2023-07-14 14:36:46 +0000

    hello

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - prints all single numbers of base 10
+ * followed by a new line, using putchar
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+putchar(n);
+}
+putchar('\n);
+return (0);
+}
