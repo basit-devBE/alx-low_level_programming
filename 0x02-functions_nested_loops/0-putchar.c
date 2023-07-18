9fa4b0e9b332aecdb95b13908a25232a570b3ed5
kkrasta <basitmohammed362@gmail.com>
2023-07-18 12:30:17 +0000

    hi

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,8 @@
-#include "main.h"
+#include <unistd.h>
+
+/* Function prototype */
+int _putchar(char c);
+
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -12,7 +16,15 @@ while (*sh)
 _putchar(*sh);
 sh++;
 }
+
 _putchar('\n');
 
 return (0);
 }
+
+/* _putchar function definition */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
