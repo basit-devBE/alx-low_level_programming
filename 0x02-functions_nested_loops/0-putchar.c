39027431462e46d5231d913b489dcb2cef884546
kkrasta <basitmohammed362@gmail.com>
2023-07-18 12:50:19 +0000

    jii

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,27 +1,25 @@
 /*
-* File: 0-putchar.c 
+*File:0-putchar.c
 *
 */
 
-#include "main.h" 
-#include <stdio.h>
-/** 
-* main-print putchar followed by a new line
-* 
+#include "main.h"
+#include<stdio.h>
+/**
+*main-print putchar followed by a new line
+*
 *Return :always 0
 */
-int main(void) 
+int main(void)
 {
-putchar('_' );
-putchar ('p' );
-putchar ('u' );
-putchar ('t' );
-putchar ('c' );
-putchar ('h' );
-putchar ('a' );
-putchar ('r' );
+putchar('_');
+putchar ('p');
+putchar ('u');
+putchar ('t');
+putchar ('c');
+putchar ('h');
+putchar ('a');
+putchar ('r');
 putchar ('\n');
-     
-return (0) ;
-} 
-        
+return (0);
+}
