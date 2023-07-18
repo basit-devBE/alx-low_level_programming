016205867e16776510753a322b6299b135852f37
kkrasta <basitmohammed362@gmail.com>
2023-07-18 11:27:49 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - Entry point of the program
+ * Description - prints a program putchar
+ * Return 0 (Representing sucess in the code)
+ */
+int main(void)
+{
+printf("\n_putchar\n");
+return (0);
+}
