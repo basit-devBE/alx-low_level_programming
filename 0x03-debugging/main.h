b1bb33fe1725981b958fbd41c34b66d88b268bb7
kkrasta <basitmohammed362@gmail.com>
2023-07-19 12:10:41 +0000

    hello

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,19 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i)
-{
-if (i > 0)
-{
-printf("\n %d is a positive number\n", i);
-}
-else if (i <  0)
-{
-printf("\n %d is a negative integer\n", i);
-}
-else
-{
-printf("\n %d is zero\n", i );
-}
-}
 
-#endif
+
+#endif /* MAIN_H */
