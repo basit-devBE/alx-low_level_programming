5324478c317e64987f2e608579da5983c9634884
kkrasta <basitmohammed362@gmail.com>
2023-07-20 13:23:40 +0000

    hello

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers from 0-9
+ *
+ * Description: prints numbers from 0-9
+ * Rturn: void
+ *
+ */
+
+void print_numbers(void)
+{
+char num;
+
+for (num = 0; num <= 57; num++)
+{
+_putchar(num);
+}
+_putchar('\n');
+}
