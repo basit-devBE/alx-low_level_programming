3cdcc9e8ec74f4c7c2445dbbd4a963293b57b291
kkrasta <basitmohammed362@gmail.com>
2023-07-20 12:30:56 +0000

    jajd

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,14 @@
 
 /**
  * _isupper - checks if a number is in upper or lower case
- *@c - character to be checked
- *Description: Prints the alphabet with _putchae
- * Return: 1 if c is uppercase or 0 if otherwise
- */
+ * @c - character to be checked
+ * Description: Prints the alphabet with _putchae
+ * Return: void
+ * */
 
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
