65cfa59e67fcaf5575d82d3fcb37b45a87075719
kkrasta <basitmohammed362@gmail.com>
2023-07-31 15:16:25 +0000

    hi

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_chessboard - Entry point
@@ -14,7 +15,7 @@ int j;
 for (i = 0; i < 8; i++)
 {
 for (j = 0; j < 8; j++)
-_putchar(a[i][j]);
-_putchar('\n');
+putchar(a[i][j]);
+putchar('\n');
 }
 }
