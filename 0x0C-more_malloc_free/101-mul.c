42d2224a8286bdf314bbb8820a4daf30c8dd039e
basit <basitmohammed362@gmail.com>
2023-08-10 14:19:51 +0000

    hey

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -120,12 +120,13 @@ for (ti = 0; e[ti]; ti++)
 _putchar(e[ti]);
 exit(98);
 }
+
 for (l1 = 0; argv[1][l1]; l1++)
 ;
 for (l2 = 0; argv[2][l2]; l2++)
-		;
+;
 ln = l1 + l2 + 1;
-a = malloc(ln * sizeof(char));
+a = malloc(ln *sizeof(char));
 if (a == NULL)
 {
 for (ti = 0; e[ti]; ti++)
